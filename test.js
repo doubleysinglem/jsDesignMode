ac6ee40449b9e936e662efba5334ab30868a64f3
ken <15952039685@163.com>
2016-07-08 13:53:16 +0800

    ..

--- test.js
+++ test.js
@@ -1,59 +1,85 @@
-var Conf=(function(){
-        var conf={
-            MAX_NUM:100,
-            MAX_NUM2:200,
-            MAX_NUM3:300
-        };
-        return {
-            get:function(name){
-                return conf[name]?conf[name]:null;
+var Waiter = function(){
+    var dfd = [],
+        doneArr = [],
+        failArr = [],
+        slice = [].slice,
+        that = this;
+    //监控对象
+    var Promise = function(){
+        this.resolved = false;
+        this.rejected = false;
+    }
+    Promise.prototype = {
+        resolve : function(){
+            this.resolved = true;
+            if(!dfd.length)
+                return;
+            for(var i = dfd.length-1;i>=0;i--){
+                if(dfd[i]&&!dfd[i].resolved || dfd[i].rejected){
+                    return;
+                }
+                dfd.slice(i,1);
+            }
+            _exec(doneArr);
+        },
+        reject : function(){
+            this.rejected = true;
+            if(!dfd.length)
+                return;
+            dfd.splice(0);
+            _exec(failArr);
+        }
+    }
+    that.Deferred = function(){
+        return new Promise();
+    }
+    function _exec(arr){
+        var i = 0,
+            len = arr.length;
+        for(;i<len;i++){
+            try{
+                arr[i]&&arr[i]();
+            }catch(e){}
+        }
+    }
+    that.when = function(){
+        dfd = slice.call(arguments);
+        var i = dfd.length;
+        for(--i;i>=0;i--){
+            if(!dfd[i]||dfd[i].resolved||dfd[i].rejected||!dfd[i] instanceof Promise){
+                dfd.splice(i,1);
             }
         }
-    })()
-
-
-    console.log(Conf.get('MAX_NUM'))
- var Conf2=(function(){
-        var conf={
-            MAX_NUM:100,
-            MAX_NUM2:200,
-            MAX_NUM3:300
-        };
-        function Single(){
-             return {
-                get:function(name){
-                   return  conf[name]?conf[name]:null;
-                }
-             }
-        }
-        return function(){
-            return Single();
-        }
-    })()
-
- console.log(Conf2().get('MAX_NUM'))
-
-
-
-var Vistor = (function(){
-  return {
-    splice : function(){
-      var args = Array.prototype.splice.call(arguments,1);
-      return Array.prototype.splice.apply(arguments[0], args);
-    },
-    push : function(){
-      var len = arguments[0].length || 0;
-      var args = this.splice(arguments,1);
-      arguments[0].length = len + arguments.length -1;
-
-      return Array.prototype.push.apply(arguments[0],args);
-    },
-    pop : function(){
-      return Array.prototype.pop.apply(arguments[0]);
+        return that;
     }
-  }
-})()
+    that.done = function(){
+        doneArr = doneArr.concat(slice.call(arguments));
+        return that;
+    }
+    that.fail = function(){
+        failArr = failArr.concat(slice.call(arguments));
+        return that;
+    }
+}
 
-var a ={};
-console.log(Vistor.push(a,1,2,3,4));
-console.log(a);
+//使用
+var waiter = new Waiter();
+var first = function(){
+    var dtd = waiter.Deferred();
+    setTimeout(function(){
+        console.log('first finish');
+        dtd.reject();
+    },5000)
+    return dtd;
+}();
+var second = function(){
+    var dtd = waiter.Deferred();
+    setTimeout(function(){
+        console.log('second  finish');
+        dtd.resolve();
+    },10000)
+    return dtd;
+}();
+
+waiter.when(first, second).done(function(){console.log('success')},function(){console.log('success again')}).fail(function(){console.log('fail')});
+
